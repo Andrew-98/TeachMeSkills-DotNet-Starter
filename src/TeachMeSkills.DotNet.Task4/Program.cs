8406f3da6a425c83ba27a6a996b6a5f4b05c8f83
AndrewKh <akholupov@gmail.com>
2020-09-18 21:48:46 +0300

    feat: add solution of task 4

---
+++ src/TeachMeSkills.DotNet.Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.141592653M;
+            decimal r = 3.5M;
+            decimal h = 4.75M;
+            decimal v = pi * r * r * h;
+            decimal s = 2 * pi * r * (r + h);
+            Console.WriteLine(v);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
