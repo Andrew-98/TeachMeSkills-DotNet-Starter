004b13fde41a3c0ed9885ed16066f5dd75f2c095
AndrewKh <akholupov@gmail.com>
2020-09-18 21:55:49 +0300

    feat: add solution of task 5

---
+++ src/TeachMeSkills.DotNet.Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int c = a + b;
+            int d = a - b;
+            int e = a / b;
+            int f = a * b;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.ReadLine();
+        }
+    }
+}
